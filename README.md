d42e9f33f9f9e3f16fcb0db4e1909d7780f9a49d
Ayush Singh <119442358+SingAyush@users.noreply.github.com>
2025-07-17 17:18:07 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # SummarizeText
+
+## WorkFlow
+
+#update config.yaml
+#update params.yaml
+#update entity
+#update configuration manager in src config 
+#update components
+#update pipeline
+#update main.py
+#update app.py
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,14 @@
+from TextSummarizer.pipeline.stage1_data_ingestion import DataIngestionTrainingPipeline
+from TextSummarizer.logging import logger
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">> stage {STAGE_NAME} started")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f"stage {STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
